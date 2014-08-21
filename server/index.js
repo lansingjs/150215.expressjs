a93f451d547db20b55ef371578dbe92bd34d863b
Leo Dion <leogdion@brightdigit.com>
2014-08-21 10:08:21 -0400

    added another web page and static pages

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
-var http = require('http');
-var fs = require('fs');
-// __dirname
-// __filename
-var data = fs.readFileSync(__dirname + '/../static/index.html');
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(data);
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/../static'));
+
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
 });
 
-server.listen(8000);
+app.get('/question', function (req, res) {
+  res.send("42");
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
