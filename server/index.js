7ceb9cae14eca7bc834875355f6684a9ed51df00
Leo Dion <leogdion@brightdigit.com>
2015-02-16 14:30:16 -0500

    hello world

--- server/index.js
+++ server/index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/../static'));
-
-app.get('/hello.txt', function(req, res){
+app.get('/', function(req, res){
   res.send('Hello World');
-});
-
-app.get('/question', function (req, res) {
-  res.send("42");
 });
 
 var server = app.listen(3000, function() {
